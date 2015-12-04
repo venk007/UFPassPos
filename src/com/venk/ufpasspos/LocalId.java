f80ebedc5423fdd4c0f3423092ca04b1b75d8a81
venk007 <liwenkeq@sina.com>
2015-12-05 00:59:42 +0800

    Maybe Need LocalId , less&simple
    Signed-off-by: venk007 <liwenkeq@sina.com>

---
+++ src/com/venk/ufpasspos/LocalId.java
@@ -0,0 +1,19 @@
+package com.venk.ufpasspos;
+
+import java.util.List;
+
+/**
+ * @Description: 省市名称转换地区编码
+ * @author: Venk007
+ * @date: 2015年12月4日下午11:30:59 
+ *
+ */
+public class LocalId {
+	
+	public static List<String> localId(){
+		
+		
+		return null;	
+	}
+
+}
